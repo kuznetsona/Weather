e2f700c9c6f65c7af7b443ad8b2063e6af33f296
Daria <54943927+kuznetsona@users.noreply.github.com>
2022-12-21 13:12:31 +0300

    #3 C to F

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,5 +28,36 @@ public class Main {
 
     }
 
+    public double convertCToF() {
+        double temperature = getTemperature(scanner, printStream);
+        return temperatureCtoF(temperature);
+    }
+
+    public double getTemperature(Scanner scanner, PrintStream printStream) {
+        double temperature;
+        while (true) {
+            printStream.println("Print:");
+            String value = scanner.nextLine();
+
+            try {
+                if (value.contains(".")) {
+                    temperature = Double.parseDouble(value);
+                } else {
+                    temperature = Integer.parseInt(value);
+                }
+                break;
+            } catch (Exception e) {
+                printStream.println("Wrong input value");
+            }
+        }
+        return temperature;
+    }
+
+    public static double temperatureCtoF(double temperature) {
+        return temperature * 9.0 / 5.0 + 32.0;
+    }
+
+
+
 
 }
